b7b917d403a9a051fcbe273c2b24ab33637b625e
Dokamiii <eduardofranciscoln110@gmail.com>
2025-02-16 23:57:32 -0300

    Data, idioma e linguagem.

--- scr/Main.java
+++ scr/Main.java
@@ -1,19 +1,26 @@
 package scr;
 
+import java.awt.*;
+import java.sql.SQLOutput;
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
 
-    static String nome;
-
     public static void main(String[] args) {
+        System.out.println("Olá mundo");
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é ");
+        System.out.println(relogio.toString());
+
+        Locale locale = new Locale("Portugues", "pt");
+        System.out.println("Linguagem: " + Locale.getDefault());
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("Screen width = " + d.width);
+        System.out.println("Screen height = " + d.height);
     }
 
-    void declaraNome(){
-        System.out.println(nome);
-        nome = "Eduardo";
-    }
-
-    String getNome(){
-        return nome;
-    }
 
 }
