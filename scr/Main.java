44491bdafeed8478cbc195c2f56423216138fa84
Dokamiii <eduardofranciscoln110@gmail.com>
2025-02-16 22:04:32 -0300

    Teste

---
+++ scr/Main.java
@@ -0,0 +1,19 @@
+package scr;
+
+public class Main {
+
+    static String nome;
+
+    public static void main(String[] args) {
+    }
+
+    void declaraNome(){
+        System.out.println(nome);
+        nome = "Eduardo";
+    }
+
+    String getNome(){
+        return nome;
+    }
+
+}
