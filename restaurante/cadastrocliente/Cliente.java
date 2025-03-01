2310af553307b5d3e8a9dc1323e9f1079868e4de
Dokamiii <eduardofranciscoln110@gmail.com>
2025-03-01 17:17:37 -0300

    Apagado a parte de Print no Cliente

--- restaurante/cadastrocliente/Cliente.java
+++ restaurante/cadastrocliente/Cliente.java
@@ -1,7 +1,5 @@
 package restaurante.cadastrocliente;
 
-import java.util.Scanner;
-
 public class Cliente {
 
     private String nomeCompleto;
@@ -36,31 +34,6 @@ public class Cliente {
 
     public void setNumeroTelefone(String numeroTelefone) {
         this.numeroTelefone = numeroTelefone;
-    }
-
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
-        System.out.println("Digite o primeiro nome");
-        String primeiroNome = teclado.nextLine();
-
-        System.out.println("Digite o segundo nome");
-        String segundoNome = teclado.nextLine();
-        String nomeCompleto = primeiroNome + " " + segundoNome;
-
-        System.out.println("Digite o CPF");
-        String cpf = teclado.nextLine();
-
-        System.out.println("Digite o numero de telefone: ");
-        String numeroTelefone = teclado.nextLine();
-
-        Cliente cliente = new Cliente(nomeCompleto, cpf, numeroTelefone);
-
-        System.out.println("Nome Completo: " + cliente.getNomeCompleto());
-        System.out.println("CPF: " + cliente.getCpf());
-        System.out.println("Numero de telefone :" + cliente.getNumerotelefone());
-
-        teclado.close();
     }
 
 }
