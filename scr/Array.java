f07b9ef23aaab93ddd5b4225b9dfd5fc8a220b02
dokamii <eduardofranciscoln110@gmail.com>
2025-03-03 22:34:54 -0300

    Media de idade

--- scr/Array.java
+++ scr/Array.java
@@ -3,14 +3,27 @@ package scr;
 public class Array {
 
     public static void main(String[] args) {
-        String [] cars = {"BMW","VOLVO","FORD"};
-        
-        for(String i : cars){
+        String[] cars = { "BMW", "VOLVO", "FORD" };
+
+        for (String i : cars) {
             System.out.println(i);
         }
-        for(int i = 0; i < cars.length;  i++){
+        for (int i = 0; i < cars.length; i++) {
             System.out.println(cars[i]);
         }
+        int ages[] = { 20, 23, 24, 32, 30, 54, 68, 82 };
+
+        float avg, sum = 0;
+
+        int length = ages.length;
+
+        for (int age : ages) {
+            sum += age;
         }
-    
+
+        avg = sum / length;
+
+        System.out.println("A media de ideade é :" + avg);
+    }
+
 }
